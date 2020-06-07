fb3889076b8e60e8430e59256219c93c738d3815
Emerson D <frenow@gmail.com>
2020-06-07 17:43:23 -0300

    initial commit

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post('/usage-end', cors(), function(req, res) {
     res.end();
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(process.env.PORT || 8080, function(){
     console.log('listening on port:' + (process.env.PORT || 3000));
 });
 
